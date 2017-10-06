b906cf9d11735726fc017e25b45bc33610c1f18b
Arakmmis <arakmmis@gmail.com>
2017-10-06 07:40:43 +0200

    Added Image Chooser Dialog to Add Contacts Activity

--- app/src/main/java/me/arakmmis/contactsapp/ui/addContact/AddContactActivity.kt
+++ app/src/main/java/me/arakmmis/contactsapp/ui/addContact/AddContactActivity.kt
@@ -1,20 +1,31 @@
 package me.arakmmis.contactsapp.ui.addContact
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AlertDialog
+import android.util.Log
 import android.view.View
+import com.aitorvs.android.allowme.AllowMe
+import com.aitorvs.android.allowme.AllowMeActivity
+import com.bumptech.glide.Glide
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
 import kotlinx.android.synthetic.main.add_contact_activity.*
+import kotlinx.android.synthetic.main.dialog_upload_pic.view.*
 import me.arakmmis.contactsapp.R
 import me.arakmmis.contactsapp.mvpcontracts.AddContactContract
+import pl.aprilapps.easyphotopicker.DefaultCallback
+import pl.aprilapps.easyphotopicker.EasyImage
+import java.io.File
 import java.util.*
 
 /**
  * Created by arakm on 10/5/2017.
  */
-class AddContactActivity : AppCompatActivity(), AddContactContract.AddContactView, DatePickerDialog.OnDateSetListener {
+class AddContactActivity : AllowMeActivity(), AddContactContract.AddContactView, DatePickerDialog.OnDateSetListener {
 
     private lateinit var presenter: AddContactContract.AddContactPresenter
+    private var profilePicFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,7 +84,73 @@ class AddContactActivity : AppCompatActivity(), AddContactContract.AddContactVie
     }
 
     fun openChooserDialog(v: View) {
+        val builder = AlertDialog.Builder(this)
 
+        val dialogView = layoutInflater.inflate(R.layout.dialog_upload_pic, null)
+        builder.setView(dialogView)
+
+        val alertDialog = builder.create()
+
+        dialogView.tv_upload_via_camera.setOnClickListener { _ ->
+            if (!AllowMe.isPermissionGranted(Manifest.permission.CAMERA)) {
+                AllowMe.Builder()
+                        .setPermissions(Manifest.permission.CAMERA)
+                        .setRationale("To take a picture the app needs access to the Camera")
+                        .setCallback({ _, result ->
+                            if (result.isGranted(Manifest.permission.CAMERA)) {
+                                EasyImage.openCamera(this, 0)
+                                alertDialog.dismiss()
+                            }
+                        }).request(1)
+            } else {
+                EasyImage.openCamera(this, 0)
+                alertDialog.dismiss()
+            }
+        }
+
+        dialogView.tv_upload_via_gallery.setOnClickListener { _ ->
+            if (!AllowMe.isPermissionGranted(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                AllowMe.Builder()
+                        .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .setRationale("To pick an image from the gallery the app needs access to the external storage")
+                        .setCallback({ _, result ->
+                            if (result.isGranted(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                                EasyImage.openGallery(this, 0)
+                                alertDialog.dismiss()
+                            }
+                        }).request(2)
+            } else {
+                EasyImage.openGallery(this, 0)
+                alertDialog.dismiss()
+            }
+        }
+
+        alertDialog.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, object : DefaultCallback() {
+            override fun onImagesPicked(imageFiles: MutableList<File>, source: EasyImage.ImageSource?, type: Int) {
+                Glide.with(this@AddContactActivity)
+                        .load(imageFiles[0])
+                        .into(iv_contact_pic)
+
+                profilePicFile = imageFiles[0]
+            }
+
+            override fun onImagePickerError(e: Exception?, source: EasyImage.ImageSource?, type: Int) {
+                Log.e("ACA: EIError", e?.message)
+            }
+
+            override fun onCanceled(source: EasyImage.ImageSource?, type: Int) {
+                if (source == EasyImage.ImageSource.CAMERA) {
+                    val photoFile = EasyImage.lastlyTakenButCanceledPhoto(this@AddContactActivity)
+                    photoFile?.delete()
+                }
+            }
+        })
     }
 
     fun addContact(v: View) {
